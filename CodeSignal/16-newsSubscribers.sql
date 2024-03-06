01ebd00a9907f2bb1900811a34600921398b3c8d
Phurinat Puekkham <phurinaix@gmail.com>
2024-03-06 23:17:27 +0700

    Create 16-newsSubscribers.sql

---
+++ CodeSignal/16-newsSubscribers.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE solution()
+BEGIN
+    SELECT DISTINCT subscriber FROM full_year WHERE newspaper LIKE '%Daily%'
+    UNION
+    SELECT DISTINCT subscriber FROM half_year WHERE newspaper LIKE '%Daily%'
+    ORDER BY subscriber;
+END
