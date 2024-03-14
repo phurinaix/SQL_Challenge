9c925dc7373faa54e4a02e4a2f0c6b988898112e
Phurinat Puekkham <phurinaix@gmail.com>
2024-03-14 23:43:52 +0700

    Create 42-alarmClocks.sql

---
+++ CodeSignal/42-alarmClocks.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE solution()
+BEGIN
+	DROP TABLE IF EXISTS output;
+    CREATE TABLE output(alarm_date datetime);
+    
+	SET @intial_date = (SELECT input_date FROM userInput);
+    SET @date = @intial_date;
+    
+    WHILE YEAR(@intial_date) = YEAR(@date) DO
+        INSERT INTO output(alarm_date) SELECT @intial_date;
+        SET @intial_date = DATE_ADD(@intial_date, INTERVAL 7 DAY);
+    END WHILE;
+    
+    SELECT * FROM output;
+END
